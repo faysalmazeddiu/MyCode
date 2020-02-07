f4c0c1da7fb40cecc08353db6dd1238a3b01ef48
unknown <faysalmazeddiu@gmail.com>
2020-02-07 16:08:42 +0600

    index.php file updated in master Baranch

--- index.php
+++ index.php
@@ -12,6 +12,11 @@
 
 		echo "Name of student =".$name.". and his id is =".$id;
 
+		$name="Shaharior Nafis";
+		$id="112-35-205";
+
+		echo "Name of student =".$name.". and his id is =".$id;
+
 
 	?>
 
