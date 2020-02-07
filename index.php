ba13d5216047e6389f15dc3a05d4e7ff94b5bea0
unknown <faysalmazeddiu@gmail.com>
2020-02-07 15:54:22 +0600

    the change is for NewBranch index,php

--- index.php
+++ index.php
@@ -13,6 +13,11 @@
 		echo "Name of student =".$name.". and his id is =".$id;
 
 
+		// for Branch two
+		$name="Mahir Hossain"; // Branch two
+		$id="112-35-204";
+
+		echo "Name of student =".$name.". and his id is =".$id;
 	?>
 
 </body>
