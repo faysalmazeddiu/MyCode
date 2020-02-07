dadba4e2fcfcaed707252947ec10c7d4e4ef6d14
unknown <faysalmazeddiu@gmail.com>
2020-02-07 15:39:11 +0600

    version_2

--- index.php
+++ index.php
@@ -13,6 +13,10 @@
 		echo "Name of student =".$name.". and his id is =".$id;
 
 
+		$name="Mahir Hossain";
+		$id="112-35-204";
+
+		echo "Name of student =".$name.". and his id is =".$id;
 	?>
 
 </body>
